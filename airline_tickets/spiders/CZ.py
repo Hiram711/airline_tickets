5b3cf26bac549a3343be08cf3d1631962385f7f1
张杰 <jie.zhang8@luckyair.net>
2019-02-18 12:12:50 +0800

    airline CZ spider completed

--- airline_tickets/spiders/CZ.py
+++ airline_tickets/spiders/CZ.py
@@ -52,18 +52,13 @@ class CzSpider(scrapy.Spider):
                 dep_city = segment.dep_airport.code.upper()
                 arv_city = segment.arv_airport.code.upper()
                 date_str = (now + timedelta(days=i)).strftime('%Y-%m-%d')
-                airline_url = 'http://b2c.csair.com/B2C40/newTrips/static/main/page/booking/index.html?t=S&c1={0}&c2={1}&d1={2}&at=1&ct=0&it=0'.format(dep_city, arv_city, date_str)
-                # # this is for Selenium
-                # yield scrapy.Request(airline_url, callback=self.parse, dont_filter=True,
-                #                      meta={'dep_airport_id': segment.dep_airport.id,
-                #                            'arv_airport_id': segment.arv_airport.id,
-                #                            'dep_date': (now + timedelta(days=i)).strftime('%Y%m%d')})
-
+                airline_url = 'http://b2c.csair.com/B2C40/newTrips/static/main/page/booking/index.html?t=S&c1={0}&c2={1}&d1={2}&at=1&ct=0&it=0'.format(
+                    dep_city, arv_city, date_str)
                 # this is for Splash
                 yield SplashRequest(airline_url, callback=self.parse, endpoint='execute',
                                     args={
                                         'lua_source': script,
-                                        'wait': 7
+                                        'wait': 10
                                     },
                                     meta={'dep_airport_id': segment.dep_airport.id,
                                           'arv_airport_id': segment.arv_airport.id,
@@ -71,12 +66,16 @@ class CzSpider(scrapy.Spider):
 
     def parse(self, response):
         soup = BeautifulSoup(response.text, 'html5lib')
+        if soup.find('h3', text='验证码'):
+            self.logger.error("Blocked when crawling %s" % response.url)
+            return
         l_flt = soup.find_all(class_='zls-flight-cell')
         get_time = datetime.now()
-        re_time = re.compile(r'\d{2}:\d{2}')
-        re_discount = re.compile(r'.*折')
-        re_price = re.compile('[0-9.,]+')
+        re_price = re.compile('¥[0-9.,]+')
+        l_price_class1 = ['头等舱', '公务舱', '明珠经济舱', '经济舱']
         for flt in l_flt:
+            if flt.find(class_='transicon tooltip-trigger'):
+                continue
             dep_airport_id = response.request.meta.get('dep_airport_id')
             arv_airport_id = response.request.meta.get('arv_airport_id')
             dep_date = response.request.meta.get('dep_date')
@@ -86,59 +85,45 @@ class CzSpider(scrapy.Spider):
             arv_time = re.findall(r'[0-9]{2}[:][0-9]{2}', flt.find_all(class_='zls-flgtime-arr')[0].text.strip())[0]
             flt_time = flt.find(class_='zls-flg-time').text.replace('h', '小时').replace('m', '分钟'). \
                 replace('H', '小时').replace('M', '分钟')
-            item = TicketItem()
-            item['company_id'] = self.company.id
-            item['dep_airport_id'] = dep_airport_id
-            item['arv_airport_id'] = arv_airport_id
-            item['dep_date'] = dep_date
-            item['flt_no'] = flt_no
-            item['airplane_type'] = airplane_type
-            item['dep_time'] = dep_time
-            item['arv_time'] = arv_time
-            item['flt_time'] = flt_time
-            item['create_date'] = get_time.strftime('%Y%m%d%H%M%S')
-            yield item
-            # is_direct = True
-            # transfer_city = None
-            # if flt.select_one('.summary .info .mid .zzjtzd').get_text() == '经停':
-            #     is_direct = False
-            #     transfer_city = flt.select_one('.summary .info .mid .zz').get_text().strip()
-            # is_shared = False
-            # share_company = None
-            # share_flt_no = None
-            # if flt.select_one('.summary .title').find_all('span')[0].string.strip() != '东方航空':
-            #     is_shared = True
-            #     share_company = flt.select_one('.summary .title').find_all('span')[0].string.strip()
-            #     share_flt_no = flt_no
-            # price_list_meta = flt.select('.body .product-list dl')
-            # for price_row in price_list_meta:
-            #     item = TicketItem()
-            #     item['company_id'] = self.company.id
-            #     item['dep_airport_id'] = dep_airport_id
-            #     item['arv_airport_id'] = arv_airport_id
-            #     item['dep_date'] = dep_date
-            #     item['flt_no'] = flt_no
-            #     item['airplane_type'] = airplane_type
-            #     item['dep_time'] = dep_time
-            #     item['arv_time'] = arv_time
-            #     item['flt_time'] = flt_time
-            #     item['is_direct'] = is_direct
-            #     item['transfer_city'] = transfer_city
-            #     item['is_shared'] = is_shared
-            #     item['share_company'] = share_company
-            #     item['share_flt_no'] = share_flt_no
-            #     item['price_type1'] = price_row.parent.attrs['data-type']
-            #     item['price_type2'] = price_row.find_all('dt')[0].get_text()
-            #     price_info = price_row.select_one('dd.p-p').get_text()
-            #     if re_discount.match(price_info):
-            #         item['discount'] = float(
-            #             re_price.findall(re_discount.findall(price_info)[0].strip())[0]) / 10
-            #         item['price'] = int(re.sub(r',{1}', '', re_price.findall(price_info)[1].strip()))
-            #     else:
-            #         item['discount'] = None
-            #         item['price'] = int(re.sub(r',{1}', '', re_price.findall(price_info)[0].strip()))
-            #     item['create_date'] = get_time.strftime('%Y%m%d%H%M%S')
-            #     yield item
+            is_direct = True
+            transfer_city = None
+            mid_info = flt.select_one('.zls-trans')
+            if mid_info:
+                is_direct = False
+                transfer_city = re.findall(r'([^：]+)$', mid_info)[0].replace(' ', '')
+            is_shared = False
+            share_company = None
+            share_flt_no = None
+            share_info = flt.find(attrs={'data-tip-class': 'share-show'})
+            if share_info:
+                is_shared = True
+                share_company = share_info.attrs['data-share'].split('#')[0]
+                share_flt_no = share_info.attrs['data-share'].split('#')[2]
+            price_list_meta = flt.find_all(class_='zls-price-cell')
+            for price_row in price_list_meta:
+                item = TicketItem()
+                item['company_id'] = self.company.id
+                item['dep_airport_id'] = dep_airport_id
+                item['arv_airport_id'] = arv_airport_id
+                item['dep_date'] = dep_date
+                item['flt_no'] = flt_no
+                item['airplane_type'] = airplane_type
+                item['dep_time'] = dep_time
+                item['arv_time'] = arv_time
+                item['flt_time'] = flt_time
+                item['is_direct'] = is_direct
+                item['transfer_city'] = transfer_city
+                item['is_shared'] = is_shared
+                item['share_company'] = share_company
+                item['share_flt_no'] = share_flt_no
+                item['price_type1'] = l_price_class1[int(price_row.parent.parent.attrs['data-cabin'])]
+                item['price_type2'] = price_row.select_one('.cabin-info .cabin-name').get_text()
+                item['discount'] = float(
+                    price_row.select_one('.cabin-other').find_all('li')[1].get_text().replace('折', '')) / 10
+                item['price'] = int(
+                    re_price.findall(price_row.select_one('.cabin-price-info').get_text())[0].replace('¥', ''))
+                item['create_date'] = get_time.strftime('%Y%m%d%H%M%S')
+                yield item
 
     def closed(self, reason):
         self.session.close()
--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 if __name__ == '__main__':
-    # # debug single spider
-    # sys.path.append(os.path.abspath(__file__))
-    # execute('scrapy crawl MU'.split())
+    # debug single spider
+    sys.path.append(os.path.abspath(__file__))
+    execute('scrapy crawl CZ'.split())
 
-    # execute all the spider
-    os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'airline_tickets.settings')  # must load the project settings first
-    process = CrawlerProcess(get_project_settings())
-    for spider_name in process.spider_loader.list():
-        # print(spider_name)
-        process.crawl(spider_name)
-    process.start()
+    # # execute all the spider
+    # os.environ.setdefault('SCRAPY_SETTINGS_MODULE', 'airline_tickets.settings')  # must load the project settings first
+    # process = CrawlerProcess(get_project_settings())
+    # for spider_name in process.spider_loader.list():
+    #     # print(spider_name)
+    #     process.crawl(spider_name)
+    # process.start()
